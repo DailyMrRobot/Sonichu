f6ee6fd083f160a3a4cbffa10ace5f4598c307f0
Ronnie Johnson <87240554+DailyMrRobot@users.noreply.github.com>
2022-11-26 20:48:24 -0500

    Update Sonichu.py

--- Sonichu.py
+++ Sonichu.py
@@ -29,31 +29,10 @@ bot = commands.Bot(command_prefix='.')
 async def nine_nine(ctx):
     brooklyn_99_quotes = [
         "IF YOU DO NOT COME CLEAN RIGHT NOW I AM GOING TO GET MY FATHER IN ON THIS FURTHER AND YOU ARE GOING TO GET FUCKIN' ARRESTED THROWN IN JAIL AND YOU WILL NOT GET TO LIVE THE PEACEFUL HAPPY LIFE WITH CASEY THAT I WOULD LIKE YOU TO LIVE BECAUSE CASEY REALLY CARES FOR YOU MAN!",
-
+        "Tell me why, Im stuck as a virgin with rage",
     ]
 
     response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-@bot.command(name='hat')
-async def hat(ctx):
-    hat_names = ['Booty Hunter',
-    'Lifeguard On Booty',
-    'Groom',
-    'Free Hat Limit 1',
-    'Nice Legs, What Time They Open?',
-    'Tell Your Titties To Stop Staring At My Eyes!',    
-    "I'm Did Asbestos I Can",
-    "Too Funk To Druck",
-    "I ♡ Cock Fighting",
-    "Hold My Hat While I Have Sex With Your Wife",
-    "Rock Out With Your Jock Out Daytona Beach 1986",
-    "No Habla Jibber Jabber",
-    "Breath If You're Horny",
-    "Divorce Is Strong With This One",
-     "The Big E",
-
-    ]
-    response = random.choice(hat_names)
     await ctx.send(response)
 
 @bot.command(pass_context=True)
@@ -64,15 +43,15 @@ async def join(ctx):
         source = FFmpegPCMAudio('ChrisChan.wav')
         player = voice.play(source)
     else:
-        await ctx.send("Boy, get your ass in a voice channel before I whup that ass!")
+        await ctx.send("Get in this voice channel before I shove your underwear down your throat")
 
 @bot.command(pass_context=True)
 async def leave(ctx):
     if (ctx.voice_client):
         await ctx.guild.voice_client.disconnect()
-        await ctx.send("I done did left that bitch you feel me?")
+        await ctx.send("Outta the way, Im chopping down the internet.")
     else:
-        await ctx.send("I aint in a voice channel if your dumbass aint in there boy.")       
+        await ctx.send("Bye")       
 
 
 
